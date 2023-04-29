18305eaaf133496f55be9cdbb8802aff2535c86f
artem <artem.yurev.1997@gmail.com>
2023-04-29 20:49:17 +0300

    task

--- src/by/pvt/thread/Auto.java
+++ src/by/pvt/thread/Auto.java
@@ -1,24 +1,6 @@
 package by.pvt.thread;
 
 
-public class Auto implements Runnable {
-    private Long millis;
-    private String name;
+public class Auto  {
 
-    public Auto(Long millis, String name) {
-        this.millis = millis;
-        this.name = name;
-    }
-
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(millis);
-            for (int i = 0; i < 50; i++) {
-                System.out.println(name + " едет " + i + " раз");
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
